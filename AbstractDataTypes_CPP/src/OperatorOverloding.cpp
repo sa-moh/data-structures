94c3133fcf0ea097080606c4e82014dd1214c347
saba <sabaahrbi91@gmail.com>
2025-11-24 10:58:10 +0330

    test first way!

--- AbstractDataTypes_CPP/src/OperatorOverloding.cpp
+++ AbstractDataTypes_CPP/src/OperatorOverloding.cpp
@@ -6,7 +6,7 @@
 #include "iostream"
 using namespace std;
 
-template <class T>
+
 class x{
     int evalue;
 public:
@@ -19,5 +19,12 @@ public:
     }
 };
 int main(){
-    cout << "hey";
+    x x2(2);
+    x x5(5);
+    if(x2 < x5)
+        cout << "x2 < x5" << endl;
+    else
+        cout << "x2 >= x5" << endl;
+    return 0;
+
 }
